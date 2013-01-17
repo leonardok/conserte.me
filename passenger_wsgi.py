22ddc83c1aa9a8b1d2ce4a61ed1e995a7fc4912f
Leonardo Korndörfer <leokorndorfer@gmail.com>
2013-01-17 17:31:15 -0200

    adding passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,17 +1,13 @@
 import sys, os
-cwd = os.getcwd()
-sys.path.append(cwd)
-sys.path.append(cwd + '/project')  #You must add your project here or 500
+ 
+sys.path.append(os.getcwd())
+sys.path.append(os.getcwd() + '/conserte_me')
 
-#Switch to new python
-#You may try to replace $HOME with your actual path
-if sys.version < "2.7.3": os.execl("$HOME/websites/conserte.me/env/bin/python",
-"python2.7.3", *sys.argv)
+sys.path.insert(0,'/home/conserte_me/env/bin')
+sys.path.insert(0,'/home/conserte_me/env/lib/python2.6/site-packages/django')
+sys.path.insert(0,'/home/conserte_me/env/lib/python2.6/site-packages')
 
-sys.path.insert(0,'$HOME/websites/conserte.me/env/bin')
-sys.path.insert(0,'$HOME/websites/conserte.me/env/lib/python2.7/site-packages/django')
-sys.path.insert(0,'$HOME/websites/conserte.me/env/lib/python2.7/site-packages')
-
-os.environ['DJANGO_SETTINGS_MODULE'] = "conserte.me.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "conserte_me.settings"
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
+
