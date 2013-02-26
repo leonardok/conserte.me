a197dcc0b1037423c9aca223e62e9739181e1646
Leonardo Korndörfer <leokorndorfer@gmail.com>
2013-02-26 16:38:50 -0300

    changing modal windows

--- public/assets/js/issues.new.js
+++ public/assets/js/issues.new.js
@@ -35,75 +35,80 @@ function init_map(){
 }
 
 
-$( "#dialog-form" ).dialog({
-   autoOpen: false,
-   width: 350,
-   modal: true,
-   buttons: {
-     Cancel: function() {
-       $( this ).dialog( "close" );
-     },
-     "Enviar": function() {
-       $("#new-issue-form").hide();
-       $("#dialog-form").height('200px');
-       $("#dialog-form").spin();
 
-       var bValid = true;
-       $("#errors_list").text('');
-       if( !$('#name').val() ) {
-         $("#errors_list").append('<li>Nome n&atilde;o pode ser vazio.</li>');
-         $('.alert.alert-error').show();
-         bValid = false;
-       }
-
-       if( !$('#description').val() ) {
-         $("#errors_list").append('<li>Descri&ccedil;&atilde;o n&atilde;o pode ser vazio.</li>');
-         $('.alert.alert-error').show();
-         bValid = false;
-       }
-       if ( !bValid ) {
-	 $("#dialog-form").spin(false);
-         $("#dialog-form").height('auto');
-         $("#new-issue-form").show();
-         return false;
-       }
-
-       $.ajax ({
-             type: "POST",
-             //the url where you want to sent the userName and password to
-             url: '/api/issues/',
-             dataType: 'json',
-             async: false,
-             
-             data: '{ "issue": { "name": "'+$('#name').val()+'", "description": "'+$('#description').val()+'", "latitude": "' + $('#latitude').val() + '", "longitude": "' + $('#longitude').val() + '" } }',
-             success: function (data) {
-             	url = "/issues/" + data.issue.id;
-             	$(location).attr('href',url);
-
-             	$("#dialog-form").dialog( "close" );
-             }
-         })
-
-     },
-   },
-   close: function() {
-     //allFields.val( "" ).removeClass( "ui-state-error" );
-     $('#map_canvas').gmap('clear', 'markers');
-   }
- });
-
-
-$( "#create-user" )
-	.button()
-	.click(function() {
-	$( "#dialog-form" ).dialog( "open" );
+$("#new_problem_message_checkbox").click(function(){
+	if ( $("#new_problem_message_checkbox").prop('checked') ){
+		$.cookie('problem_message_checkbox', false, { expires: 30 });
+	}
 });
+
 
 $('#janela_novo_problema').on('hidden', function () {
 	$('#map_canvas').gmap('clear', 'markers');
 })
 
+$('#send_new_issue').click(function(){
+	// do not process if this is disabled
+	if ( $('#send_new_issue').hasClass("disabled") ) { return }
+
+	var bValid = true;
+	$("#errors_list").text('');
+	if( !$('#name').val() ) {
+		$("#errors_list").append('<li>Nome n&atilde;o pode ser vazio.</li>');
+		$('.alert.alert-error').show();
+		bValid = false;
+	}
+
+	if( !$('#description').val() ) {
+		$("#errors_list").append('<li>Descri&ccedil;&atilde;o n&atilde;o pode ser vazio.</li>');
+		$('.alert.alert-error').show();
+		bValid = false;
+	}
+	if ( !bValid ) {
+		$("#dialog-form").spin(false);
+		$("#dialog-form").height('auto');
+		$("#new-issue-form").show();
+		return false;
+	}
+
+	$('#send_new_issue').addClass('disabled');
+	$('#cancel_new_issue').addClass('disabled');
+	
+	$('#new_problem_form').hide();
+	$('.modal-body').spin();
+
+	data_to_post = '{ "issue": { "name": "'+$('#name').val()+'", "description": "'
+		+ $('#description').val()+'", "latitude": "' + $('#latitude').val()
+		+ '", "longitude": "' + $('#longitude').val() + '" } }';
+
+	$.ajax ({
+		type: "POST",
+		//the url where you want to sent the userName and password to
+		url: '/api/issues/',
+		dataType: 'json',
+		async: true,
+
+		data: data_to_post,
+		success: function (data) {
+			url = "/issues/" + data.issue.id;
+			$(location).attr('href',url);
+		}
+	}).error(function(){
+		$("#errors_list").append('<li>Ocorreu um erro. Por favor repita a operação.</li>');
+		$('.alert.alert-error').show();
+
+		$('#send_new_issue').removeClass('disabled');
+		$('#cancel_new_issue').removeClass('disabled');
+		
+		$('#new_problem_form').show();
+		$('.modal-body').spin(false);
+	});
+});
+
 $(document).ready(function() {
 	init_map();
-	$('#how_to_mark').modal('show');
+
+	if ($.cookie('problem_message_checkbox') == null) {
+		$('#how_to_mark').modal('show');
+	}
 });
