5b475a6b480fb91188e518cc17e8b65ba7d78b58
Leonardo Korndörfer <leokorndorfer@gmail.com>
2013-03-05 18:31:44 -0300

    fixing tabs in haml files

--- conserte_me/settings.py
+++ conserte_me/settings.py
@@ -105,6 +105,8 @@ SECRET_KEY = 's#_3ozw)&amp;(h+2=^t26h1w&amp;z47!pd#0ptsz!*+&amp;nex7vsul_o&amp;i
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
+    'hamlpy.template.loaders.HamlPyFilesystemLoader',
+    'hamlpy.template.loaders.HamlPyAppDirectoriesLoader',
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
